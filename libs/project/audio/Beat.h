0aa2f1c4a2dc4029daf1eaf87a6fa69f3bb22abf
zaqks <imzakbro@gmail.com>
2024-03-06 21:00:40 +0100

    beat

--- libs/project/audio/Beat.h
+++ libs/project/audio/Beat.h
@@ -3,7 +3,8 @@ typedef struct {
   int bpm;
   char *beats;
 
-  int tick;//keeps track of the current bit
+  int len;  // bits num
+  int indx; // keeps track of the current bit
 } Beat;
 
 void initSound() {
@@ -14,12 +15,13 @@ void initSound() {
 
 Beat *initBeat() {
   Beat *beat = (Beat *)malloc(sizeof(Beat));
+
   // load the mp3
   char *bgPath =
       "data/levels/level1/sounds/track/TwoStepsFromHellArchangel.mp3";
   beat->bg = Mix_LoadWAV(bgPath);
 
-  // load the beats
+  // load the beats file
   char *btsPath = "data/levels/level1/sounds/track/beats2.txt";
   FILE *btsFile;
 
@@ -37,12 +39,38 @@ Beat *initBeat() {
     }
   }
   fclose(btsFile);
+  //
+
+  beat->indx = 0;
+  beat->len = strlen(beat->beats);
 
   return beat;
 }
 
 int getMoment(Beat *beat) {
+  if (beat->indx < beat->len) {
 
+    char *current;
+    current = beat->beats + beat->indx;
+
+    if (*current != '|' &
+        beat->indx != beat->len - 1) // check if !(ure at the end & char is '|')
+    {
+
+      if (*current == '|') // skip the division
+      {
+        beat->indx += 1;
+        current += 1;
+      }
+      beat->indx += 1;
+
+      if (*current != '0') {
+        return 1;
+      }
+    }
+  }
+
+  return 0;
 }
 
 void playBg(Beat *beat) {
